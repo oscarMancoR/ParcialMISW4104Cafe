5e53808332e20755388a7232fe8304dd2b3aadce
oscarmancoR <o.manco@uniandes.edu.co>
2023-11-05 16:37:45 -0500

    configuracion de la prueba de componente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ListarCafeComponent } from './cafes/listar-cafe/listar-cafe.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule,HttpClientModule],
+    declarations: [AppComponent,ListarCafeComponent]
   }));
 
   it('should create the app', () => {
@@ -20,10 +22,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ParcialMISW4104');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ParcialMISW4104 app is running!');
-  });
+
 });
--- src/app/cafes/listar-cafe/listar-cafe.component.spec.ts
+++ src/app/cafes/listar-cafe/listar-cafe.component.spec.ts
@@ -4,14 +4,22 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { ListarCafeComponent } from './listar-cafe.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Cafe } from '../cafe';
+import { CafeService } from '../cafe.service';
+import { faker } from '@faker-js/faker';
+
 
 describe('ListarCafeComponent', () => {
   let component: ListarCafeComponent;
   let fixture: ComponentFixture<ListarCafeComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListarCafeComponent ]
+      imports: [HttpClientModule],
+      declarations: [ ListarCafeComponent ],
+      providers: [CafeService]
     })
     .compileComponents();
   }));
@@ -19,10 +27,33 @@ describe('ListarCafeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarCafeComponent);
     component = fixture.componentInstance;
+
+    const tipos:string[] = ["Interior", "Interior", "Exterior"];
+    for(let i = 0; i < 3; i++) {
+      const cafe = new Cafe(
+          faker.number.int(),
+         faker.string.alpha(20),
+         faker.string.alpha(20),
+         faker.string.alpha(20),
+         faker.string.alpha(10),
+        faker.number.int(),
+        faker.string.alpha(10)
+      );
+      component.cafes.push(cafe);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+ 
+
+  it('should have 3 <td> tags', () => {
+    expect(debug.queryAll(By.css('tbody tr'))).toHaveSize(3)
+  });
+ 
+ 
 });
